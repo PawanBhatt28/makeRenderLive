342fbc3dd6fd7da296c84a9c289c01dc9404dcf0
saksham.srivastava <saksham.srivastava@kapturecrm.com>
2024-10-04 17:15:27 +0530

    added Logger

--- src/main/java/dev/pawanbhatt/ownown/Service.java
+++ src/main/java/dev/pawanbhatt/ownown/Service.java
@@ -2,6 +2,8 @@ package dev.pawanbhatt.ownown;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import java.util.Objects;
 
 @Component
 public class Service {
+
+    public static final Logger LOGGER= LoggerFactory.getLogger(Service.class);
 
     @Autowired
     Dao dao;
@@ -28,14 +32,14 @@ public class Service {
                 try {
                     RestTemplate restTemplate = new RestTemplate();
                     dao.setData(Objects.requireNonNull(restTemplate.getForObject(url, String.class)).toString());
-                    System.out.println("PONG");
+                    LOGGER.info(url);
                     Thread.sleep(2000);
                 } catch (Exception error) {
-                    System.out.println(error.getMessage());
+                    LOGGER.error(error.getMessage());
                     Thread.currentThread().interrupt();
                 }
             }
-//        };
+        //        };
 //        thread.start();
 //        return new ResponseEntity<>( "Cannot send request", HttpStatus.BAD_REQUEST);
     }
